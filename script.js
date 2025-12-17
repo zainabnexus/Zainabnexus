39a2fe3b1e0897adbf0710cfb74c38fb8da1b7c1
zainabnexus <zainabnexus.works@gmail.com>
2025-12-17 21:24:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function setLang(lang){
-document.documentElement.lang = lang;
 
-document.querySelectorAll("[data-en]").forEach(el=>{
-el.innerText = el.getAttribute("data-" + lang);
-});
-
-if(lang === "ur"){
-document.body.setAttribute("lang","ur");
-}else{
-document.body.removeAttribute("lang");
-}
-}
